ffa666cd3eee6444a405518b8d02c5c5deec0b68
Mathijs Baaijens <mathijs@mathijsbaaijens.nl>
2023-11-24 22:12:02 +0100

    Initial commit.

---
+++ train.py
@@ -0,0 +1,47 @@
+from functools import partial
+import argparse, os, sys, random, logging
+import numpy as np
+from sklearn import svm
+import pickle
+
+# Set random seeds for repeatable results
+RANDOM_SEED = 3
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+
+# Load files
+parser = argparse.ArgumentParser(description='Train custom ML model')
+parser.add_argument('--data-directory', type=str, required=True)
+parser.add_argument('--max-iter', type=int, required=True, default=-1)
+parser.add_argument('--out-directory', type=str, required=True)
+
+args, _ = parser.parse_known_args()
+
+out_directory = args.out_directory
+
+if not os.path.exists(out_directory):
+    os.mkdir(out_directory)
+
+# grab train/test set
+X_train = np.load(os.path.join(args.data_directory, 'X_split_train.npy'))
+Y_train = np.load(os.path.join(args.data_directory, 'Y_split_train.npy'))
+X_test = np.load(os.path.join(args.data_directory, 'X_split_test.npy'))
+Y_test = np.load(os.path.join(args.data_directory, 'Y_split_test.npy'))
+
+# sparse representation of the labels (1-based)
+Y_train = np.argmax(Y_train, axis=1)
+Y_test = np.argmax(Y_test, axis=1)
+
+print('Training linear SVM classifier on', str(X_train.shape[0]), 'inputs...')
+clf = svm.SVC(kernel='linear', max_iter=args.max_iter)
+clf.fit(X_train, Y_train)
+
+print('Training linear SVM classifier OK')
+print('')
+
+print('Mean accuracy (training set):', clf.score(X_train, Y_train))
+print('Mean accuracy (validation set):', clf.score(X_test, Y_test))
+print('')
+
+with open(os.path.join(args.out_directory, 'model.pkl'),'wb') as f:
+    pickle.dump(clf,f)
